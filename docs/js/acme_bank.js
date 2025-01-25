c0f766f35afe84d14de2238d22581d569383ab65
Jos Valdivia <1594177+Josaphat@users.noreply.github.com>
2025-01-25 16:37:21 -0500

    Format money strings in bank and stonks

--- docs/js/acme_bank.js
+++ docs/js/acme_bank.js
@@ -1,10 +1,9 @@
-//
 function load_contents() {
     const accountBalanceEl = document.querySelector("#account-balance");
     const notificationsAreaEl = document.querySelector("#notifications-area")
 
     var accountBalance = -1000000;
-    accountBalanceEl.textContent = accountBalance.toFixed(2);
+    accountBalanceEl.textContent = "$" + accountBalance.toLocaleString("en-US", {minimumFractionDigits: 2, maximumFractionDigits: 2});
 
     if (accountBalance < 0) {
 	notificationsAreaEl.innerHTML = "<h2>This account is past due</h2><h2>Final notice of delinquency</h2><h2>Final Final Notice</h2><h2>Account is overdrawn</h2><h2>Don't forget to pay capital gains tax</h2><h2>Final Final Final Notice</h2>";
