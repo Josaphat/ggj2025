1fbf9b53d3de6e71896357f919b7e92109e5523f
cgiugno <67064875+cgiugno@users.noreply.github.com>
2025-01-25 13:49:58 -0500

    close and change response to email

---
+++ docs/js/startingEmail.js
@@ -0,0 +1,17 @@
+const emailTextOptions = {
+    yes: "Yeah, absolutely! How do I start?",
+    no: 'Nah, I\'m good. I\'d rather go bankrupt :/'
+}
+function onOptionChange(arg) {
+    const optionPicked = arg.options[arg.selectedIndex].value
+
+    const emailText = document.querySelector('#email-message-container')
+    emailText.innerHTML = emailTextOptions[optionPicked
+
+    ]
+}
+function sendEmail (windowNum) {
+    const emailWindow = document.querySelector(`#email-popup-${windowNum}`)
+
+    emailWindow.style.setProperty('display', 'none')
+}
