232576306cc25d05cd7b8fc03f6103b608b44aa1
Jos Valdivia <1594177+Josaphat@users.noreply.github.com>
2025-01-25 21:39:40 -0500

    WIP: Terrence Archive

--- docs/js/terrencearchive.js
+++ docs/js/terrencearchive.js
@@ -1,1 +1,21 @@
 // terrencearchive.js
+var once = false;
+
+window.onload = (event) => {
+    var buttonElement = document.querySelector("#contact-us-button");
+    if (!buttonElement) return;
+    var body = document.querySelector("body");
+
+    buttonElement.onclick = (event) => {
+	if (once) {
+	    return;
+	}
+	once = true;
+
+	var answer = document.createElement("p");
+	answer.innerHTML = `Opening bell tomorrow, everything is going to hit the fan.<br>
+You want to be in G.E., ATT, and JNJ, and basically nothing else.<br>
+- 1337`;
+    body.appendChild(answer);
+    };
+};
